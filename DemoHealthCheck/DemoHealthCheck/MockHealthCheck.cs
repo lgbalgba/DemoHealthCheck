4bdc1e7ebc64f86846488dc4c4bc4b48b6ad056d
lucas <lgbalgba@msn.com>
2020-01-07 15:26:34 -0300

    Demo HealthCheck

---
+++ DemoHealthCheck/DemoHealthCheck/MockHealthCheck.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoHealthCheck
+{
+    public class MockHealthCheck : IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            HttpClient client = new HttpClient();
+            var result = await client.GetAsync("https://localhost:44357/api/values");
+
+            if (!result.IsSuccessStatusCode)
+                return HealthCheckResult.Unhealthy(string.Empty);
+            
+            return HealthCheckResult.Healthy(string.Empty);
+        }
+    }
+}
